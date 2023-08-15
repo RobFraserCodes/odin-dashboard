ad454bdbb49798ab1a46ced06baec7a9cf8ea4d6
Robert Fraser <99823868+RobFraserCodes@users.noreply.github.com>
2023-08-15 08:58:37 +0100

    Very Basic Layout
    building the grid

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,45 @@ export default function Home() {
       <div className='col-span-9 bg-slate-300'>
         {/* Header */}
         <div className='flex flex-col'>
+        <div className="grid gap-4">
+          <div className="col-span-12 bg-white">
+            header
+          </div>
+
+          {/* Projects */}
+          <div className='col-span-8'>
+            Projects
+            <div className='grid grid-cols-2 gap-4'>
+              <div className='col-span-1 bg-white'>
+                card 1
+              </div>
+              <div className='col-span-1 bg-white'>
+                card 2
+              </div>
+            </div>
+          </div>
+
+          {/* Announcements & Trending */}
+          <div className='col-span-4'>
+            <div className='flex flex-col'>
+              Announcements
+              <div className='bg-white'>
+                Announcements
+              </div>
+              Trending
+              <div className='bg-white'>
+                Trending
+              </div>
+            </div>
+          </div>
 
         </div>
+        </div>
+
+      {/* Footer */}
+      <div className='bg-slate-500'>
+        Footer
+      </div>
 
       </div>
     
